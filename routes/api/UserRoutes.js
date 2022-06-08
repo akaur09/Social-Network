fcb5d2582d74027a1966bdb50e14a9207c0a02ae
akaur091 <amniderkaur.09@gmail.com>
2022-06-07 23:58:41 -0700

    add routes for users

--- routes/api/UserRoutes.js
+++ routes/api/UserRoutes.js
@@ -11,7 +11,10 @@ const {
     deleteFriend
 } = require ('../../controllers/UserController');
 // call routes for creating user
-
+router.route('/').get(getAllUsers).post(createUser);
 // call route for updating user
-
+router.route ('/:id').get(getUserById).put(updateUser).delete(deleteFriend);
 // call route for friends
+router.route('/:id:friends').post(addFriend).delete(deleteFriend);
+
+module.exports = router;
