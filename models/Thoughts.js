46d83d87731cbfe04473d72f96fd90c786346bfe
akaur091 <amniderkaur.09@gmail.com>
2022-06-07 17:19:22 -0700

    add psuedo code in thoughts model based on the homework readme

---
+++ models/Thoughts.js
@@ -0,0 +1,15 @@
+// call on shema and mongo to create model
+// create a schema model function called Thoughts
+    // for thought text 
+        // take it in as a string
+        // make it required
+        // limit the charaters to be w/in 1-280
+    // have a created at 
+        // have the date
+        // set it to default to current timestamp
+        // use a get method to format the timestamp
+    // username
+        // take it in as a string
+        // require it
+    // reactions
+        // take in an array that is created with the reaction schema
