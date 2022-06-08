bed9a9e1a5ea8ed224bb62a2fecc76be8e869916
akaur091 <amniderkaur.09@gmail.com>
2022-06-07 23:03:12 -0700

    add code for getting all users

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -9,5 +9,17 @@ const UserController = {
         User.create(body)
         .then(dbUserData => res.join(dbUserData))
         .catch(err => res.status (400).json(err))
+    },
+    // get users
+    getAllUsers (req, res) {
+        User.find({})
+        .populate({path: 'Thoughts', select: '__v'})
+        .populate({path: 'friends', select: '__v'})
+        .select('-__v')
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
     }
 }
