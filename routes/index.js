5123da3aa79175e0fd22fc62797231029deada7b
akaur091 <amniderkaur.09@gmail.com>
2022-06-07 21:51:43 -0700

    call routes in main index.js

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,6 @@
-// require express
+// require express router
+const router = require('express').Router();
 // import routes
+const apiRoutes = require('./api');
 // export modules
+module.exports = router;
