7e049a717b44555e409086e8eb2395f0215dd93a
akaur091 <amniderkaur.09@gmail.com>
2022-06-07 22:50:12 -0700

    add code following psuedocode

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,3 +1,6 @@
 // require response from express
+const res = require('express/lib/response');
 // require users
+const {User} = require('../models');
 // create funcution for user
+const UserController = {}
