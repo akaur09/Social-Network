8ccc855f862c9fb1935f4631219ece83b3d0c5b7
akaur091 <amniderkaur.09@gmail.com>
2022-06-07 23:22:44 -0700

    add psuedocode for creating thoguhts controller

--- controllers/ThoughtsController.js
+++ controllers/ThoughtsController.js
@@ -1,3 +1,13 @@
 // require models
 const {Thoughts, User} = require('../models');
 // create a function for creating thoughts
+const ThoughtsController = {
+    // create thoughts
+    // get all thougths
+    // get only one thought
+    // edit thought
+    // delete thought
+    // add a reaction
+    // delete a reaction
+}
+module.exports = ThoughtsController;
