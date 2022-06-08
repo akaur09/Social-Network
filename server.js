3ff966cd99cb829f56487ca435306baec998aa50
akaur091 <amniderkaur.09@gmail.com>
2022-06-07 22:04:52 -0700

    add code to connect mongoose

--- server.js
+++ server.js
@@ -3,5 +3,10 @@ const express = require('express');
 // require mongoose
 const mongoose = require('mongoose');
 // connect mongoose
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost-Social-Network',{
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology:true
+})
 // log mongoose queries
 // have app listen
