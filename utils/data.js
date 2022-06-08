6f2984dd7eb9c83e2aff80bb9ab3d09caab1f69a
akaur091 <amniderkaur.09@gmail.com>
2022-06-08 00:05:14 -0700

    add some data to be tested

---
+++ utils/data.js
@@ -0,0 +1,14 @@
+var User = [
+    {
+        username: "Loki",
+        email: "loki@gmail.com"
+    }
+]
+var Thoughts = [
+    {
+        thoughtText: "I want ice cream",
+        username: "loki"
+    }
+]
+
+module.exports = {User, Thoughts};
