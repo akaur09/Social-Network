57db56d07560a4a7ff32bd51561697d2da344577
akaur091 <amniderkaur.09@gmail.com>
2022-06-07 22:38:31 -0700

    add code to user schema

--- models/User.js
+++ models/User.js
@@ -1,17 +1,34 @@
 // create model for users
 // require mongo to create schema
+const {Schema, Model} = require('mongoose');
+const { User } = require('.');
 // create the schema model function called User
-    // for username make sure to have:
-        // take it in as a string
-        // set it to be unique
-        // require it
-        // trim white space
-    // for email:
-        // take in in as a string
-        // require it
-        // set it to be unique
-        // use mongoose to validate the email to be an email address
+const userSchema = new Schema(
+    {
+        // for username make sure to have:
+        username:{
+            // take it in as a string
+
+            // set it to be unique
+            // require it
+            // trim white space  
+        },
+     // for email:
+        email: {
+            // take in in as a string
+            // require it
+            // set it to be unique
+            // use mongoose to validate the email to be an email address
+        },
     // for thoughts:
-        // have an array of id referencing the Thoughts model
+        Thoughts: {
+            // have an array of id referencing the Thoughts model
+        },
     // for friends
-        // have an array of id referencing to the User model
+        friends: {
+            // have an array of id referencing to the User model
+        }
+        
+    }
+)
+module.exports = User;     
