c1162cbbe366e8098a7a70705dce084ab62cd899
akaur091 <amniderkaur.09@gmail.com>
2022-06-07 23:11:09 -0700

    write code for updating a user

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -33,8 +33,24 @@ const UserController = {
                 res.status(404).json({message: 'no user with this id'});
                 return;
             }
+            res.json(dbUserData)
         })
         .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        });
+    },
+    // update a user
+    updateUser ({params, body}, res){
+        User.findOneAndUpdate({_id: params.id}, body, {new: true, runValidators: true})
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({message: 'no user with this id'});
+                return;
+            }
+            res.json(dbUserData)
+        })
+        .catch (err => {
             console.log(err);
             res.status(400).json(err);
         });
