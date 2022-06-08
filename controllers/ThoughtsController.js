2b3d453af2cfbdadf7334651f51f66ad140f073d
akaur091 <amniderkaur.09@gmail.com>
2022-06-07 23:27:10 -0700

    add code for creating a thought

--- controllers/ThoughtsController.js
+++ controllers/ThoughtsController.js
@@ -3,6 +3,20 @@ const {Thoughts, User} = require('../models');
 // create a function for creating thoughts
 const ThoughtsController = {
     // create thoughts
+    createThoughts({params, body}, res){
+        Thoughts.create(body)
+        .then (({_id}) => {
+            return User.findOneAndUpdate({_id: params.userId}, {$push: {thoughts: _id}}, {new: true});
+        })
+        .then (dbThoughtsData => {
+            if (!dbThoughtsData) {
+                res.status(404).json({message:'no thought with this id'})
+                return;
+            }
+            res.json(dbThoughtsData)
+        })
+        .catch (err => res.json(err));
+    }
     // get all thougths
     // get only one thought
     // edit thought
