446502049bb12b1827cc89b67498f2bb8bfa0777
akaur091 <amniderkaur.09@gmail.com>
2022-06-07 23:51:48 -0700

    add routes for thoughts

--- routes/api/ThoughtsRoutes.js
+++ routes/api/ThoughtsRoutes.js
@@ -1,5 +1,23 @@
 // require express
+const router = require('express').Router();
 // reqquires controller components
+const {
+    getAllThoughts,
+    getThoughtsById,
+    createThoughts,
+    updateThoughts,
+    deleteThoughts,
+    addReaction,
+    deleteReaction
+} = require ('../../controllers/ThoughtsController');
 // call routes for thoughts
+router.route('/').get(getAllThoughts);
+router.route('/:id').get(getThoughtsById).put(updateThoughts).delete(deleteThoughts);
+router.route('/:userId').post(createThoughts);
+
 // call routes for reactions
+router.route('/:thoughtsId/reactions').post(addReaction);
 // call routes for deleting a reaction
+router.route('/:thoughtsId/reactions/reactionId').delete(deleteReaction);
+
+module.exports =router;
