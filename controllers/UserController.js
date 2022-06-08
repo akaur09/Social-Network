4a37e51e4c94504154335f3092720a4fb0d360f1
akaur091 <amniderkaur.09@gmail.com>
2022-06-07 22:59:23 -0700

    create code for creating a user

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -3,4 +3,11 @@ const res = require('express/lib/response');
 // require users
 const {User} = require('../models');
 // create funcution for user
-const UserController = {}
+const UserController = {
+    // create user
+    createUser ({body}, res) {
+        User.create(body)
+        .then(dbUserData => res.join(dbUserData))
+        .catch(err => res.status (400).json(err))
+    }
+}
