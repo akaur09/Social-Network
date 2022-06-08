dcc87c0954819393e93a91e194aacff6a007b6ad
akaur091 <amniderkaur.09@gmail.com>
2022-06-07 21:49:45 -0700

    add code following psuedo code

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,5 +1,11 @@
 // require express
+const router = require("express").Router();
 // require routes
+const UserRoutes = require('./UserRoutes');
+const ThoughtsRoutes = require('./ThoughtsRoutes');
 // add user to beginning of user routes (refer to homework readme)
+router.use('./User', UserRoutes);
 // add thoughts to beginnign of user routers (refer to homework readme)
+router.use('./Thoughts', ThoughtsRoutes);
 // export modules
+module.exports = router;
